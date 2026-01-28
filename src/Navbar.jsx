e12437d2e470613fe74123f3a173c14d8e9b9a3e
Zeus <tchaipa97@gmail.com>
2026-01-28 19:41:22 +0200

    28/01/26

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,30 +1,26 @@
-import React from "react";
 import "./Navbar.css";
 import { Col, Container, Image, Row } from "react-bootstrap";
 // import { Button } from "react-bootstrap";
 import logo from "./assets/vox_logo.png";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Navbar() {
   return (
     <div className="navbar container-fluid sticky-top">
       <Image src={logo} href="" className="nav-logo" />
-
-      <a className="nav-a" href="">
-        Home
-      </a>
-      <a className="nav-a" href="">
-        About
-      </a>
-      <a className="nav-a" href="">
-        Destinations
-      </a>
-      <a className="nav-a" href="">
-        FAQ
-      </a>
-      <a className="nav-a" href="">
-        Contact
-      </a>
+      <div className="nav-links container-fluid">
+        <a className="nav-a" href="">
+          Home
+        </a>
+        <a className="nav-a" href="">
+          About
+        </a>
+        <a className="nav-a" href="">
+          Destinations
+        </a>
+        <a className="nav-a" href="">
+          Contact
+        </a>
+      </div>
     </div>
   );
 }
--- src/Packages.jsx
+++ src/Packages.jsx
@@ -1,18 +1,62 @@
-import { Card, Divider } from "@mui/material";
+import { Box, Card, Divider, Grid } from "@mui/material";
 import "./Packages.css";
 import { Button, Image } from "react-bootstrap";
-import vumba from "./assets/vumba.jpg";
-import mana from "./assets/mana.jpg";
-import matopo from "./assets/matopo.jpg";
+// import vumba from "./assets/vumba.jpg";
+// import mana from "./assets/mana.jpg";
+// import matopo from "./assets/matopo.jpg";
+import sky from "./assets/sky.jpg";
+
 function Packages() {
   return (
     <div className="packages-section container-fluid">
-      <h2 className="packages-title">Our Exclusive Tour & Travel Packages</h2>
+      <h2 className="packages-title">What We Offer</h2>
       <p>
-        Litora pede, recusandae amet aliquam. Cupiditate molestiae nec venenatis
-        at massa risus, eum fugiat conubia integer at mattis.
+        Our packages are designed to meet the needs of every traveler, from solo
+        adventures to family trips.
       </p>
-      <div className=" packages-card container-fluid">
+      <Box className="packages-box container-fluid">
+        <Grid
+          className="packages-grid container-fluid"
+          size={4}
+          container
+          spacing={2}
+        >
+          <div className="packages-content-one container-fluid">
+            <h3>Curated Tour Packages</h3>
+            <p>
+              We offer a curated selection of tour packages, catering various
+              travel styles, themes and durations. <br />
+              <br />
+              These packages are designed to help you discover the destinations
+              in Zimbabwe in an unforgettable way, whether you are looking for a
+              relaxing beach vacation, an adrenaline-pumping adventure, or a
+              cultural immersion experience.
+            </p>
+            <br />
+            <Button className="packages-button">Get Started</Button>
+          </div>
+        </Grid>
+        <Grid
+          className="packages-grid container-fluid"
+          size={4}
+          container
+          spacing={2}
+        >
+          <div className="packages-content container-fluid">
+            {/* <span>
+              We offer a curated selection of tour packages, catering various
+              travel styles, themes and durations. <br />
+              <br />
+              These packages are designed to help you discover the destinations
+              in Zimbabwe in an unforgettable way, whether you are looking for a
+              relaxing beach vacation, an adrenaline-pumping adventure, or a
+              cultural immersion experience.
+            </span> */}
+            <Image className="packages-image" src={sky}></Image>
+          </div>
+        </Grid>
+      </Box>
+      {/* <div className=" packages-card container-fluid">
         <Card>
           <Image className="packages-image" src={vumba}></Image>
           <div
@@ -56,7 +100,7 @@ function Packages() {
       <div className="packages-buttons container-fluid">
         <Button>View all Packages</Button>
         <Button>Learn More</Button>
-      </div>
+      </div> */}
     </div>
   );
 }
