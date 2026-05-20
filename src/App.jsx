6060194761933bcac9469078d9191ed412c0d2cb
Zeus <tchaipa97@gmail.com>
2026-05-20 15:14:52 +0200

    initial commit v2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbarr from "./Navbar";
-import Home from "./pages/Home";
-import Packages from "./pages/Packages";
+// import Home from "./pages/Home";
+import Packages from "./pages/PackagesPage";
 import Gallery from "./pages/Gallery";
-import ContactUs from "./pages/ContactUs";
+import ContactUs from "./pages/ContactsPage";
 
 function App() {
   return (
     <Router>
       <Navbarr />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" />
         <Route path="/packages" element={<Packages />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/contact-us" element={<ContactUs />} />
